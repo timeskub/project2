d0b5c22ff993948a465f9bcb77e9a6c28c6f6a42
timeskub <PHANUKORNS@HOTMAIL.COM>
2022-10-26 22:18:57 +0700

    done

---
+++ nd1309-p2-Decentralized-Star-Notary-Service-Starter-Code-main/README.md
@@ -0,0 +1,14 @@
+Specify the Truffle version and OpenZeppelin version used in the project.
+
+Truffle Version 5.6.2
+OpenZeppelin Version 2.3.0
+
+Your ERC-721 Token Name: Hello Uda
+Your ERC-721 Token Symbol: HU
+
+Your “Token Address” on the Sepolia Network
+0x81034058Ee8211226c6625c072590e90C79B9f62
+My Token ID: 1
+https://sepolia.etherscan.io/token/0x81034058ee8211226c6625c072590e90c79b9f62?a=1
+
+
---
+++ nd1309-p2-Decentralized-Star-Notary-Service-Starter-Code-main/app/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    sourceMapFilename: "[name].js.map"
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+  ],
+  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+  devtool: "source-map"
+};
